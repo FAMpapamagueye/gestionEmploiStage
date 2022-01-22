eac2a59bac8a272280ba93b81034c43912850c6a
tine50 <abdoussalamtine4@gmail.com>
2022-01-22 16:09:47 +0000

    Initial branch test

--- gestionEmploiStage/routes/web.php
+++ gestionEmploiStage/routes/web.php
@@ -23,19 +23,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome.index');
 });
-// // Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-// Route::get('/departements', [Departement::class,'render'])->name('departements');
-// /**
-//  * cette route permet d'afficher l'ensemble des routes d'un département
-//  */
-// Route::get('/filieres', [Filiere::class,'render'])->name('filieres');
-/**
- * Cette route permet d'afficher la liste des etudiants d'une filière
- */
+
 Route::resource('departements',DepartementController::class);
 Route::resource('promos',PromotionController::class);
 Route::resource('filieres',FiliereController::class);
