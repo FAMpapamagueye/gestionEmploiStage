b8f417373cc5f1d0f66968f6e9766a0fc226f6b8
tine50 <abdoussalamtine4@gmail.com>
2022-01-25 13:55:05 +0000

    filter à partir de la promotion

--- gestionEmploiStage/database/seeders/FiliereSeeder.php
+++ gestionEmploiStage/database/seeders/FiliereSeeder.php
@@ -15,73 +15,73 @@ class FiliereSeeder extends Seeder
     public function run()
     {
         DB::table('filieres')->insert([
-            ['libelle' => 'Developpement back-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
+            // ['libelle' => 'Developpement back-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
 
-            ['libelle' => 'Developpement front-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
+            // ['libelle' => 'Developpement front-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
 
-            ['libelle' => 'Analyse performance digital','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
+            // ['libelle' => 'Analyse performance digital','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
 
-            ['libelle' => 'Réseau IOT','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
+            // ['libelle' => 'Réseau IOT','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
 
-            ['libelle' => 'Administration base de donnée','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
-            ['libelle' => 'Gestion des données de l\entreprise','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 1,
-            ],
+            // ['libelle' => 'Administration base de donnée','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
+            // ['libelle' => 'Gestion des données de l\entreprise','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 1,
+            // ],
 
 
 
-            ['libelle' => 'Mécanique automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 2,
-            ],
+            // ['libelle' => 'Mécanique automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 2,
+            // ],
 
-            ['libelle' => 'Electricité automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 2,
-            ],
+            // ['libelle' => 'Electricité automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 2,
+            // ],
 
-            ['libelle' => 'Froid et climatisation','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 2,
-            ],
+            // ['libelle' => 'Froid et climatisation','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 2,
+            // ],
 
-            ['libelle' => 'Carroserie automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 2,
-            ],
+            // ['libelle' => 'Carroserie automobile','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 2,
+            // ],
 
 
 
             //Promotion 2
 
-            ['libelle' => 'Developpement back-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
+            // ['libelle' => 'Developpement back-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
 
-            ['libelle' => 'Developpement front-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
+            // ['libelle' => 'Developpement front-end','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
 
-            ['libelle' => 'Analyse performance digital','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
+            // ['libelle' => 'Analyse performance digital','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
 
-            ['libelle' => 'Réseau IOT','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
+            // ['libelle' => 'Réseau IOT','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
 
-            ['libelle' => 'Administration base de donnée','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
-            ['libelle' => 'Gestion des données de l\entreprise','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
-            'departement_id' => 3,
-            ],
+            // ['libelle' => 'Administration base de donnée','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
+            // ['libelle' => 'Gestion des données de l\entreprise','description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy',
+            // 'departement_id' => 3,
+            // ],
 
 
 
